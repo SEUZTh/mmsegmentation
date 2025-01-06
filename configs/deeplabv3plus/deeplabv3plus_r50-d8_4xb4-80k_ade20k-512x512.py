9bb0699214066cbb7a2a410f9bc8894d84d3072d
张天浩 <50619570+SEUZTh@users.noreply.github.com>
2025-01-06 20:04:41 +0800

    [Feature] 添加 TTPLADataset 数据集配置和相关设置

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_4xb4-80k_ade20k-512x512.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_4xb4-80k_ade20k-512x512.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/ade20k.py',
+    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/ttpla.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
 crop_size = (512, 512)
